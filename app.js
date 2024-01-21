42cae6e80173b592612d235544aaaacbaa1aa545
Alex_leop <lx.leopoldo@outlook.com>
2024-01-21 18:31:40 -0300

    configurando novo webhook

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-/*
- * Starter Project for WhatsApp Echo Bot Tutorial
- *
- * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
- *
- */
-
 "use strict";
 
-// Access token for your app
-// (copy token from DevX getting started page
-// and save it as environment variable into the .env file)
 const token = process.env.WHATSAPP_TOKEN;
 
 // Imports dependencies and set up http server
 const request = require("request"),
-  express = require("express"),
-  body_parser = require("body-parser"),
-  axios = require("axios").default,
-  app = express().use(body_parser.json()); // creates express http server
+      express = require("express"),
+      body_parser = require("body-parser"),
+      axios = require("axios").default,
+      app = express().use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 3000, () => console.log("webhook is listening"));
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 // Accepts POST requests at /webhook endpoint
 app.post("/webhook", (req, res) => {
